453162417c05452233c90f46c2e5cb921ee6a704
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-11 23:31:23 -0400

    move commiting user link out of for loop

--- dna/posts/posts.js
+++ dna/posts/posts.js
@@ -49,16 +49,16 @@ function postCreate(input) {
         }
       ]
     });
-    var userLinkHash = commit("userLink", {
-      Links: [
-        {
-          Base: App.Agent.Hash,
-          Link: postHash,
-          Tag: 'post'
-        }
-      ]
-    });
   }
+  var userLinkHash = commit("userLink", {
+    Links: [
+      {
+        Base: App.Agent.Hash,
+        Link: postHash,
+        Tag: 'post'
+      }
+    ]
+  });
   return postHash;
 }
 
