33fb4d75d6efd05f327e7b4bf90997ea44d9a56c
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-13 23:02:16 -0400

    Fix file naming scheme for post view

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,7 +4,7 @@ import { run } from '@cycle/run';
 import { makeDOMDriver } from '@cycle/dom';
 import { makeHTTPDriver } from '@cycle/http';
 import isolate from '@cycle/isolate';
-import PostView from './postView';
+import PostView from './post_view';
 
 function main(sources) {
     const postView = isolate(PostView)({props: xs.of({hash: "QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux"}), ...sources});
