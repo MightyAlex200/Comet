ede45495aabeef470bdbe7b851476fbaaa180881
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-13 20:22:15 -0400

    Added very simple PostView component, updated babel

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,14 +1,20 @@
 //@ts-check
 import xs from 'xstream';
 import { run } from '@cycle/run';
-import { makeDOMDriver, h1 } from '@cycle/dom';
+import { makeDOMDriver } from '@cycle/dom';
+import { makeHTTPDriver } from '@cycle/http';
+import isolate from '@cycle/isolate';
+import PostView from './postView';
 
-function main() {
-    // TODO: Everything
+function main(sources) {
+    const postView = isolate(PostView)({props: xs.of({hash: "QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux"}), ...sources});
+
+    return postView;
 }
 
 const drivers = {
-    DOM: makeDOMDriver('#app')
+    DOM: makeDOMDriver('#app'),
+    HTTP: makeHTTPDriver(),
 };
 
 run(main, drivers);
---
+++ ui/src/postView.js
@@ -0,0 +1,21 @@
+//@ts-check
+import xs from 'xstream';
+import { h1 } from '@cycle/dom';
+
+export default function PostView(sources) {
+    return {
+        DOM: sources.HTTP.select('posts')
+            .flatten()
+            .map(res => res.text)
+            .map(post => 
+                h1(post === null ? "null" : post)
+            )
+            .startWith(null),
+        HTTP: sources.props.map(props => ({
+            url: '/fn/posts/postRead',
+            method: 'POST',
+            category: 'posts',
+            send: `"${props.hash}"`,
+        })),
+    }
+};
