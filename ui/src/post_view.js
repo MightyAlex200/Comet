cb877ea720524041bfd8431d8cb521a76bb5068f
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-15 11:26:32 -0400

    Refactor post view

--- ui/src/post_view.js
+++ ui/src/post_view.js
@@ -3,19 +3,24 @@ import xs from 'xstream';
 import { h1 } from '@cycle/dom';
 
 export default function PostView(sources) {
+
+    const http$ = sources.props.map(props => ({
+        url: '/fn/posts/postRead',
+        method: 'POST',
+        category: 'posts',
+        send: `"${props.hash}"`,
+    }));
+
+    const dom$ = sources.HTTP.select('posts')
+        .flatten()
+        .map(res => res.text)
+        .map(post =>
+            h1(post === null ? "null" : post)
+        )
+        .startWith(null);
+
     return {
-        DOM: sources.HTTP.select('posts')
-            .flatten()
-            .map(res => res.text)
-            .map(post =>
-                h1(post === null ? "null" : post)
-            )
-            .startWith(null),
-        HTTP: sources.props.map(props => ({
-            url: '/fn/posts/postRead',
-            method: 'POST',
-            category: 'posts',
-            send: `"${props.hash}"`,
-        })),
+        DOM: dom$,
+        HTTP: http$,
     }
 };
