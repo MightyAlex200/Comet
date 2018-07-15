cedccebeda99a82a9770af3c049f6db88d3eaf3a
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-15 11:29:43 -0400

    Isolate post view

--- ui/src/post_view.js
+++ ui/src/post_view.js
@@ -4,20 +4,25 @@ import { h1 } from '@cycle/dom';
 
 export default function PostView(sources) {
 
-    const http$ = sources.props.map(props => ({
+    const http$ = sources.props.map(({ hash }) => ({
         url: '/fn/posts/postRead',
         method: 'POST',
-        category: 'posts',
-        send: `"${props.hash}"`,
+        category: `post${hash}`,
+        send: `"${hash}"`,
     }));
 
-    const dom$ = sources.HTTP.select('posts')
+    // @cycle/http's httpDriver does not actually provide isolation even with a scope
+    // This is a workaround
+    // TODO: Create a better, more elegant solution (open up issue in cycle about full http isolation?)
+
+    const dom$ = sources.props.map(({ hash }) => sources.HTTP.select(`post${hash}`)
         .flatten()
         .map(res => res.text)
         .map(post =>
             h1(post === null ? "null" : post)
         )
-        .startWith(null);
+        .startWith(null)
+    ).flatten();
 
     return {
         DOM: dom$,
