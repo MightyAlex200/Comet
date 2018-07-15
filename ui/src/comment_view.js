2e30b0f11658e63caafdefce90f25d41ab8217ad
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-14 23:29:57 -0400

    Create comment view

---
+++ ui/src/comment_view.js
@@ -0,0 +1,71 @@
+//@ts-check
+import xs from 'xstream';
+import { div } from "@cycle/dom";
+import isolate from '@cycle/isolate';
+
+export default function CommentView(sources) {
+    const commentReadHTTP$ = sources.props.map(({hash}) => 
+        ({
+            url: '/fn/comments/commentRead',
+            method: 'POST',
+            category: `commentRead${hash}`,
+            send: `"${hash}"`,
+        })
+    );
+
+    const fromHashHTTP$ = sources.props.map(({hash}) =>
+        ({
+            url: '/fn/comments/fromHash',
+            method: 'POST',
+            category: `fromHash${hash}`,
+            send: hash
+        })
+    );
+
+    const commentHTTP$ = xs.merge(commentReadHTTP$, fromHashHTTP$);
+
+    // @cycle/http's httpDriver does not actually provide isolation even with a scope
+    // This is a workaround
+    // TODO: Create a better, more elegant solution (open up issue in cycle about full http isolation?)
+
+    const commentReadDOM$ = sources.props.map(({hash}) => sources.HTTP.select(`commentRead${hash}`)
+        .flatten()
+        .map(res => res.text)
+        .map(JSON.parse)
+        .map(({content}) => content)
+        .startWith("Loading comment")
+        .map(text => div(".commentContent", text))
+    ).flatten();
+    
+    const fromHashSinksArray$ = sources.props.map(({hash}) => sources.HTTP.select(`fromHash${hash}`)
+        .flatten()
+        .map(res => res.text)
+        .map(JSON.parse)
+        .map(subCommentArray => subCommentArray.map(subComment => subComment.Hash))
+        .map(hashArray => hashArray.map(hash => isolate(CommentView, hash)({...sources, props: xs.of({hash})})))
+        .startWith([])
+    ).flatten();
+    
+    const fromHashDOM$ = fromHashSinksArray$
+        .map(sinksArray => sinksArray.map(sinks => sinks.DOM))
+        .map(dom$Array => xs.combine(...dom$Array))
+        .flatten()
+        .map(domArray => div(".subComments", {props: {style: "position: relative; left: 24px"}}, domArray));
+    
+    const subCommentHTTP$ = fromHashSinksArray$
+        .map(sinksArray => sinksArray.map(sinks => sinks.HTTP))
+        .map(http$Array => xs.merge(...http$Array))
+        .flatten();
+
+    const dom$ = xs.combine(commentReadDOM$, fromHashDOM$)
+        .map(children => div(".comment", {}, children));
+
+    const http$ = xs.merge(commentHTTP$, subCommentHTTP$);
+
+    const sinks = {
+        DOM: dom$,
+        HTTP: http$,
+    };
+    
+    return sinks;
+}
--- ui/src/index.js
+++ ui/src/index.js
@@ -5,11 +5,14 @@ import { makeDOMDriver } from '@cycle/dom';
 import { makeHTTPDriver } from '@cycle/http';
 import isolate from '@cycle/isolate';
 import PostView from './post_view';
+import CommentView from './comment_view';
 
 function main(sources) {
-    const postView = isolate(PostView)({props: xs.of({hash: "QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux"}), ...sources});
+    const viewHash = "QmacEfcQbuxDDaatxdknpZzgxWbQ9VpU24o2EJ1UhdhLQg";
+    // const postView = isolate(PostView)({props: xs.of({hash: "QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux"}), ...sources});
+    const commentView = isolate(CommentView, viewHash)({props: xs.of({hash: viewHash}), ...sources});
 
-    return postView;
+    return commentView;
 }
 
 const drivers = {
