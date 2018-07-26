54a84629c76b86ffb049ae13bbf4245030d57e1f
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-25 21:42:53 -0400

    Add basic post compose view

--- ui/src/index.js
+++ ui/src/index.js
@@ -12,11 +12,12 @@ import PostsView from './posts_view';
 import SubView from './sub_view';
 import VoteView from './vote_view';
 import MarkdownComposeView from './markdown_compose_view';
+import PostComposeView from './post_compose_view';
 
 function main(sources) {
     const postHash = 'QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux';
     // const commentHash = 'QmacEfcQbuxDDaatxdknpZzgxWbQ9VpU24o2EJ1UhdhLQg';
-    const postView = isolate(PostView)({ hash: xs.of(postHash), karmaMap: xs.of(new Proxy({}, { get: function() {return 1} })), ...sources });
+    // const postView = isolate(PostView)({ hash: xs.of(postHash), karmaMap: xs.of(new Proxy({}, { get: function() {return 1} })), ...sources });
     // const commentView = isolate(CommentView, commentHash)({ hash: xs.of(commentHash), ...sources });
     // const commentsView = isolate(CommentsView, postHash)({hash: xs.of(postHash), ...sources});
     // const tagInput = isolate(TagInput)(sources);
@@ -24,13 +25,15 @@ function main(sources) {
     // const subView = isolate(SubView)({ sub: xs.of('3562527'), ...sources})
     // const voteView = isolate(VoteView)({ hash: xs.of(postHash), ...sources });
     // const markdownComposeView = isolate(MarkdownComposeView)(sources);
+    const postComposeView = isolate(PostComposeView)(sources);
 
     // return voteView;
-    return {
-        ...postView,
-        DOM: postView.DOM.map(dom => div('.container', dom)),
-    };
+    // return {
+    //     ...postView,
+    //     DOM: postView.DOM.map(dom => div('.container', dom)),
+    // };
     // return markdownComposeView;
+    return postComposeView;
 }
 
 const drivers = {
---
+++ ui/src/post_compose_view.js
@@ -0,0 +1,50 @@
+//@ts-check
+import xs from 'xstream';
+import MarkdownComposeView from './markdown_compose_view';
+import { div, button, input } from '@cycle/dom';
+
+// TODO: subs
+export default function PostComposeView(sources) {
+    const titleInputDOM$ = xs.of(
+        input('.title-input.form-control', { attrs: { placeholder: 'Title' } })
+    );
+
+    const markdownComposeView = MarkdownComposeView(sources);
+
+    const markdownDOM$ = markdownComposeView.DOM;
+
+    const postButtonDOM$ = xs.of(
+        button('.btn.btn-primary.post-button', 'Post')
+    );
+
+    const dom$ = xs.combine(titleInputDOM$, markdownDOM$, postButtonDOM$)
+        .map(div);
+
+    const postClick$ = sources.DOM
+        .select('.post-button')
+        .events('click');
+
+    const title$ = sources.DOM
+        .select('.title-input')
+        .events('input')
+        .map(event => event.target.value);
+
+    const markdownText$ = markdownComposeView.text;
+
+    const postHTTP$ = xs.combine(title$, markdownText$, postClick$)
+        .map(([title, markdownText, clickEvent]) => ({
+            url: '/fn/posts/postCreate',
+            method: 'POST',
+            category: `postCreate`,
+            send: `{ "postEntry": { "title": "${title.replace(/"/g, '\\"')}", "content": "${markdownText.replace(/"/g, '\\"')}" }, "subs": [] }`,
+        }));
+    
+    const http$ = postHTTP$;
+
+    const sinks = {
+        DOM: dom$,
+        HTTP: http$,
+    };
+
+    return sinks;
+}
