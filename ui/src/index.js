d4b82b967f00b8178cec42b06d61e242941be27e
MightyAlex200 <quantumtraveling@gmail.com>
2018-07-26 23:31:50 -0400

    Basic routing

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,6 +3,7 @@ import xs from 'xstream';
 import { run } from '@cycle/run';
 import { makeDOMDriver, div } from '@cycle/dom';
 import { makeHTTPDriver } from '@cycle/http';
+import { makeHistoryDriver } from '@cycle/history';
 import isolate from '@cycle/isolate';
 import PostView from './post_view';
 import CommentView from './comment_view';
@@ -13,11 +14,13 @@ import SubView from './sub_view';
 import VoteView from './vote_view';
 import MarkdownComposeView from './markdown_compose_view';
 import PostComposeView from './post_compose_view';
+import BasicKarmaMap from './basic_karma_map';
+import Comet from './comet';
 
 function main(sources) {
     const postHash = 'QmTw1XpJFmVUCkiNjd63UrjRnAsziGBjLcu3YzsAFLDuux';
     // const commentHash = 'QmacEfcQbuxDDaatxdknpZzgxWbQ9VpU24o2EJ1UhdhLQg';
-    // const postView = isolate(PostView)({ hash: xs.of(postHash), karmaMap: xs.of(new Proxy({}, { get: function() {return 1} })), ...sources });
+    // const postView = isolate(PostView)({ hash: xs.of(postHash), karmaMap: xs.of(BasicKarmaMap), ...sources });
     // const commentView = isolate(CommentView, commentHash)({ hash: xs.of(commentHash), ...sources });
     // const commentsView = isolate(CommentsView, postHash)({hash: xs.of(postHash), ...sources});
     // const tagInput = isolate(TagInput)(sources);
@@ -25,7 +28,8 @@ function main(sources) {
     // const subView = isolate(SubView)({ sub: xs.of('3562527'), ...sources})
     // const voteView = isolate(VoteView)({ hash: xs.of(postHash), ...sources });
     // const markdownComposeView = isolate(MarkdownComposeView)(sources);
-    const postComposeView = isolate(PostComposeView)(sources);
+    // const postComposeView = isolate(PostComposeView)(sources);
+    const comet = Comet({ ...sources, karmaMap: xs.of(BasicKarmaMap) });
 
     // return voteView;
     // return {
@@ -33,12 +37,14 @@ function main(sources) {
     //     DOM: postView.DOM.map(dom => div('.container', dom)),
     // };
     // return markdownComposeView;
-    return postComposeView;
+    // return postComposeView;
+    return comet;
 }
 
 const drivers = {
     DOM: makeDOMDriver('#app'),
     HTTP: makeHTTPDriver(),
+    history: makeHistoryDriver(),
 };
 
 run(main, drivers);
